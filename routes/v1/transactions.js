c4d36b5d9aa3f5f62c364070a00e38c5009bd46e
GeoXhacker <mukisageophrey@gmail.com>
2023-02-11 21:55:08 +0300

    add auth before post

--- routes/v1/transactions.js
+++ routes/v1/transactions.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { transactionsController } = require("../../controllers");
+const { transactionsController, authController } = require("../../controllers");
+const catchAsync = require("../../utils/catchAsync");
+const authMiddleware = require("../../middlewares/auth");
+
+
+
 
 router
   .route("/:id")
   .delete(transactionsController.deleteOrder)
   .patch(transactionsController.confirmOrder);
 
-  router.route("/").post(transactionsController.newTransaction);
+  router.route("/").post(catchAsync(authMiddleware.authenticate),transactionsController.newTransaction);
 
 module.exports = router;
